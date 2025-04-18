118f1afa774e8f56e1668afc77ec8430d26b5014
kai.chen <chenkai@visualfine.com>
2025-04-18 09:55:48 +0800

    Fixed: 修复已知问题

--- CmBackTrace/inc/cmb_cfg.h
+++ CmBackTrace/inc/cmb_cfg.h
@@ -33,7 +33,7 @@
 #include "cmb_user_cfg.h"
 #else
 /* print line, must config by user */
-#define cmb_println(...)             SEGGER_RTT_printf(0, __VA_ARGS__);SEGGER_RTT_WriteString(0, "\r\n")/* e.g., printf(__VA_ARGS__);printf("\r\n")  or  SEGGER_RTT_printf(0, __VA_ARGS__);SEGGER_RTT_WriteString(0, "\r\n")  */
+#define cmb_println(...)             /*SEGGER_RTT_printf(0, __VA_ARGS__);SEGGER_RTT_WriteString(0, "\r\n")*/ /* e.g., printf(__VA_ARGS__);printf("\r\n")  or  SEGGER_RTT_printf(0, __VA_ARGS__);SEGGER_RTT_WriteString(0, "\r\n")  */
 /* enable bare metal(no OS) platform */
 #define CMB_USING_BARE_METAL_PLATFORM
 /* enable OS platform */
